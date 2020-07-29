f1408602cf880954fe9e63dd3e88e73b4137b4f0
ToA <nansh9815@naver.com>
2020-07-29 13:07:01 +0900

    paigination

--- Django/Pagination/app/views.py
+++ Django/Pagination/app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from .models import Blog
+from django.core.paginator import Paginator
 # Create your views here.
+def index(request):
+    blogs = Blog.objects
+    #블로그 모든 글들을 대상으로
+    blog_list=Blog.objects.all()
+    #블로그 객체 세 개를 한 페이지로 자르기
+    paginator = Paginator(blog_list,3)
+    #request된 페이지가 뭔지를 알아내고 ( request페이지를 변수에 담아냄 )
+    page = request.GET.get('page')
+    #request된 페이지를 얻어온 뒤 return 해 준다
+    posts = paginator.get_page(page)
+
+    return render(request, 'index.html',{'blogs':blogs,'posts':posts})
+
+def detail(request, id):
+    blog = get_object_or_404(Blog, pk=id)
+    return render(request, 'detail.html', {'blog':blog})
